0ae67c9cdfe148e22fdb4c01c756c36c0a8cd8e0
prantika <prantikamutsuddi47@gmail.com>
2024-08-24 11:13:07 +0600

    first

---
+++ controller/BannerStore.php
@@ -0,0 +1,101 @@
+<?php
+session_start();
+
+$title = $_REQUEST['title']?? null;
+$detail = $_REQUEST['detail'] ?? null;
+$cta_title = $_REQUEST['cta_title']?? null;
+$cta_link = $_REQUEST['cta_link']?? null;
+$video_link = $_REQUEST['video_link']?? null;
+$banner_img = $_FILES['banner_img']?? null;
+
+//  print_r($banner_img);
+//  exit();
+
+$errors = [];
+
+$extension = pathinfo($banner_img['name'])['extension'] ?? null; //* png
+
+$acceptedExtension = [
+    'jpg',
+    'png',
+];
+
+
+//* Validation Rules
+if(empty($title)){
+    $errors['title_error'] = 'title is missing!';
+}
+if(empty($detail)){
+    $errors['detail_error'] = 'detail is missing!';
+}
+
+//* PHOTO VALIDATION
+if($banner_img['size'] == 0){
+     $errors['banner_img_error'] = "Banner Image is missing";
+} else if(!in_array($extension,$acceptedExtension)){
+    $errors['banner_img_error'] = "$extension is not acceptable. Accepted types are " . join(', ',$acceptedExtension);
+}
+
+if(count($errors) > 0){
+    //* ERROR FOUND
+    $_SESSION['errors'] = $errors;
+    $_SESSION["banner"] = [];
+    header("Location: ../dashboard/banner.php");
+}else{  
+    define("UPLOAD_PATH", "../uploads");
+    if (!file_exists(UPLOAD_PATH)) {
+        mkdir(UPLOAD_PATH);
+    }
+
+
+
+
+        $fileName = 'banner-' . uniqid(). '.' .$extension;
+        move_uploaded_file($banner_img['tmp_name'], '../uploads/'.$fileName);
+        $uploadPath = "uploads/$fileName";
+
+        include "../database/env.php";
+
+
+        $query = "UPDATE `banners` SET `status` = 0";
+        mysqli_query($conn, $query);
+
+        $query = "INSERT INTO banners( title, detail,cta_title,cta_link,video_link, banner_img) VALUES ('$title','$detail','$cta_title','$cta_link','$video_link','$uploadPath')";
+
+
+        $res = mysqli_query($conn, $query);
+
+
+        if ($res) {
+          $query = "SELECT * FROM banners WHERE status = 1";
+          $result = mysqli_query($conn, $query);
+          $banner = mysqli_fetch_assoc($result);
+          $_SESSION["banner"] = $banner;
+          $_SESSION["success"] = true;
+          header("Location: ../dashboard/banner.php");
+        }
+      }
+    
+        
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
---
+++ dashboard/banner.php
@@ -0,0 +1,101 @@
+<?php
+
+  include "./include/dashboardHeader.php" ;
+  include('../database/env.php');
+?>
+              
+              
+              <div class="row">
+                
+                <div class="col-xl-4 mb-6 order-0">
+                <div class="card">
+                 <form action="../controller/BannerStore.php" enctype="multipart/form-data" method="POST">
+                 <div class="card-header py-0 pt-3 d-flex justify-content-between align-items-center">
+                    <h4>Add Banner</h4>
+                    <button class="btn btn-primary">Store</button>
+                    </div>
+                    <div class="card-body">
+                      <p> Banner Title:</p>
+                        <input  name="title" type="text" class="form-control my-2" placeholder="Banner Title"  value="<?= ($_SESSION['auth']['title']) ?? null ?>">
+                        <span class="text-danger"><?= $_SESSION['errors']['title_error'] ?? null ?></span>
+                      <p> Banner Detail:</p>
+                        <textarea name="detail" class="form-control my-2" placeholder="Banner detail"></textarea>
+                        <span class="text-danger"><?= $_SESSION['errors']['detail_error'] ?? null ?></span>
+                        <p>Cta Title:</p>  
+                        <input type="text" name="cta_title" class="form-control my-2" placeholder="Cta Title">
+                        <p> Cta Link:</p>
+                        <input type="text" name="cta_link" class="form-control my-2" placeholder="Cta Link">
+                        <p>Video Link:</p>
+                        <input type="text" name="video_link" class="form-control my-2" placeholder="Video Link">
+   
+                       <label for ="" class="d-block">
+                          Banner Image :
+                       <input type="file" name="banner_img"  class="form-control">
+                       </label>
+                       <span class="text-danger"><?= $_SESSION['errors']['banner_img_error'] ?? null ?></span>
+                        
+                    </div>
+                 </form>
+                  </div>
+                </div>
+
+
+
+ 
+                <div class="col-xl-8 mb-6 order-0">
+                  <div class="card">
+                    <div class="table-responsive">
+                        <table class="table">
+
+                           <tr>
+                              <th>#</th>
+                              <th>Banner Title</th>
+                              <th>Cta</th>
+                              <th>Video</th>
+                              <th></th>
+                            </tr>
+
+                            <tr>
+                              <td>#</td>
+                              <td>Banner Title</td>
+                              <td>Cta</td>
+                              <td>Video</td>
+                              <td></td>
+                            </tr>
+                            
+                        </table>
+                           
+                      
+                    </div>
+                    </div>
+                    
+                  </div>
+                </div>
+               
+            
+<?php
+
+include "./include/dashboardFooter.php" ;
+?>
+
+
+<?php
+
+if(isset($_SESSION['success'])){
+?>
+<script>
+    Toast.fire({
+  icon: "success",
+  title: "Banner Updated successfully"
+  });
+</script>
+
+
+
+<?php
+}
+
+unset($_SESSION['errors']);
+unset($_SESSION["success"]);
+?>
+
